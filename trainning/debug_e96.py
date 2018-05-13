4b7fa3a1e8a279b03a6fbeeed410cc3c76197dae
NHRD <naohisa.harada@gmail.com>
2018-05-13 17:58:56 +0900

    a

--- trainning/debug_e96.py
+++ trainning/debug_e96.py
@@ -1,28 +1,29 @@
-e96 = [1,1,2]
-magnitude = [1,1,2]
-target_sum = float(input("Input target value:"))
-target_num = float(input("Input target num:"))
-a_det = 0
-b_det = 0
-fixed_val = 100
-def_target = 2
+e96 = [10.0, 10.2, 10.5, 100]
+magnitude = [1,10,100]
+target_sum = float(input("Input target sum value:"))
+target_pgain = float(input("Input target pre-gain val:"))
+r1_det = 0
+r2_det = 0
+rmpu = 800
+dif_target = 200
 
-for i in e96:
+for k in magnitude:
     for j in e96:
-        for k in magnitude:
-            ii = i/k
-            jj = j/k
+        for i in magnitude:
+            ii = i * k/10
+            jj = j * k/10
             sum_val = ii + jj
-            print(sum_val)
-            
-            if sum_val > target_sum:
-                tag_cal = (ii + jj)/fixed_val + ii + jj
-                diff = (tag_cal - target_num)
+            #print(sum_val)
 
-                if diff < def_target:
+            if sum_val >= target_sum:
+                tag_cal = ii * jj /(ii + jj) + rmpu
+                diff = abs(tag_cal - target_pgain)
+                print(tag_cal)
+
+                if diff <= dif_target:
                     def_target = diff
-                    a_det = i
-                    b_det = j
+                    r1_det = ii
+                    r2_det = jj
 
-print("Target nums are {} and {}.".format(a_det, b_det))
+print("Target nums are {} and {}.".format(r1_det, r2_det))
                 
