5b9c76c9ec4f9d0e1e67f030370fce1502cd294d
NHRD <naohisa.harada@gmail.com>
2018-05-13 20:45:22 +0900

    a

---
+++ chapt21/test3.py
@@ -0,0 +1,27 @@
+class Queue:
+
+    def __init__(self):
+        self.items = []
+    
+    def is_empty(self):
+        return self.items == []
+    
+    def enqueue(self, item):
+        self.items.insert(0, item)
+
+    def dequeue(self):
+        return self.items.pop()
+
+    def size(self):
+        return len(self.items)
+    
+fifo = Queue()
+
+for i in range(5):
+    fifo.enqueue(i)
+        
+while fifo.size():
+    print(fifo.dequeue())
+
+print()
+print(fifo.size())
